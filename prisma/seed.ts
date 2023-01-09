582b079ec530cea8a1c92e49c436494cf13c3e3d
Rade58 <bajic.rade2@gmail.com>
2023-01-09 18:15:50 +0100

    🦉

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,1 +1,62 @@
 import { hashPassword } from "@/lib/auth";
+import { db } from "@/lib/db";
+
+import { TASK_STATUS } from "@prisma/client";
+
+const getRandomTaskStatus = () => {
+  const statuses = Object.values(TASK_STATUS);
+  return statuses[Math.floor(Math.random() * statuses.length)];
+};
+
+async function main() {
+  const user = await db.user.upsert({
+    where: {
+      email: "bonk@foo.com",
+    },
+    update: {},
+    create: {
+      email: "bonk@foo.com",
+      firstName: "Bonk",
+      lastName: "Shibarium",
+      password: await hashPassword("satoshiNakamoto"),
+      projects: {
+        create: new Array(6).fill(1).map((_, i) => ({
+          name: `Project ${i}`,
+          due: new Date(2023, 1, 9),
+        })),
+      },
+    },
+    include: {
+      projects: true,
+    },
+  });
+
+  const tasks = await Promise.all(
+    user.projects.map((project) => {
+      db.task.createMany({
+        data: new Array(10).fill(1).map((_, i) => {
+          return {
+            name: `Task ${i}`,
+            ownerId: user.id,
+            projectId: project.id,
+            description: `Hello world foo bar everything from Task ${i}`,
+            status: getRandomTaskStatus(),
+          };
+        }),
+      });
+    })
+  );
+
+  console.log({ user, tasks });
+}
+
+//
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await db.$disconnect();
+    process.exit(1);
+  });
