01e1b3de461361ceead5ddbfa2c19f3587945229
Rade58 <bajic.rade2@gmail.com>
2023-01-10 20:55:38 +0100

    🦉

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -13,11 +13,11 @@ const AuthRootLayout: FC<Props> = ({ children }) => {
   return (
     <html lang="en" className={inter.variable}>
       <head />
-      <body className="h-screen w-screen rainbow-mesh p-6"></body>
-
-      <GlassPane className="w-full h-full flex items-center justify-center">
-        {children}
-      </GlassPane>
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center justify-center">
+          {children}
+        </GlassPane>
+      </body>
     </html>
   );
 };
