8f57a18e8e726248cf04cf63a5fd297675ace409
Rade58 <bajic.rade2@gmail.com>
2023-01-11 23:48:21 +0100

    🦉

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -5,11 +5,23 @@ import { getUserFromCookie } from "@/lib/auth";
 import { db } from "@/lib/db";
 import delay from "@/lib/delay";
 
+import Greetings from "@/components/Greetings";
+import GreetingsSkeleton from "@/components/GreetingsSkeleton";
+
 export default function HomeDashboardPage() {
   return (
     <div className="h-full overflow-y-auto pr-6 w-1/1">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
-        <div className="flex-1 grow flex">{/** greetings here */}</div>
+        <div className="flex-1 grow flex">
+          {/** greetings here */}
+          <Suspense fallback={<GreetingsSkeleton />}>
+            {/* ERROR SAYS THAT SERVER VCOMPONENT
+            BECAUSE IT RETURNS PROMISE CAN'T BE USED IN SUSPENSE
+            BUT EVERYTHING WORKS OK */}
+            {/* @ts-ignore */}
+            <Greetings />
+          </Suspense>
+        </div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
           {/** projects map here */}
           <div className="w-1/3 p-3">{/* new project here */}</div>
