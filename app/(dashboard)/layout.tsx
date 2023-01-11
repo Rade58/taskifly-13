420216208ac607c0abdb0a1a0819a3fcbd486dba
Rade58 <bajic.rade2@gmail.com>
2023-01-12 00:14:23 +0100

    🦉

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -13,7 +13,7 @@ const DashboardRootLayout: FC<Props> = ({ children }) => {
   return (
     <html lang="en" className={inter.variable}>
       <head />
-      <body className="h-screen w-screen rainbow-mesh p-6">
+      <body className="h-screen w-screen candy-mesh p-6">
         <GlassPane className="w-full h-full flex items-center justify-center">
           {children}
         </GlassPane>
