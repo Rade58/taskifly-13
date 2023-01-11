af2ed54a0cd43aa2d419fc8506d61af57f732317
Rade58 <bajic.rade2@gmail.com>
2023-01-11 16:24:57 +0100

    🦉

---
+++ pages/api/register.ts
@@ -0,0 +1,40 @@
+import type { NextApiHandler } from "next";
+import { db } from "@/lib/db";
+import { createJWT, hashPassword } from "@/lib/auth";
+import { serialize } from "cookie";
+
+const handler: NextApiHandler = async (req, res) => {
+  if (req.method === "POST") {
+    const user = await db.user.create({
+      data: {
+        email: req.body.email,
+        password: await hashPassword(req.body.password),
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+      },
+    });
+
+    const jwt = await createJWT({
+      email: user.email,
+      id: user.id,
+    });
+
+    res.setHeader(
+      "Set-Cookie",
+      serialize(process.env.COOKIE_NAME as string, jwt, {
+        httpOnly: true,
+        path: "/",
+        // EXPIRES AFTER 7 DAYS
+        maxAge: 7 * 24 * 60 * 60,
+      })
+    );
+
+    res.status(201);
+    res.end();
+  } else {
+    res.status(401);
+    res.end();
+  }
+};
+
+export default handler;
