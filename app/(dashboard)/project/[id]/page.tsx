ab9522387065fdb9d5bc6be876ad77cf20a02edd
Rade58 <bajic.rade2@gmail.com>
2023-01-13 16:55:02 +0100

    🦉

--- app/(dashboard)/project/[id]/page.tsx
+++ app/(dashboard)/project/[id]/page.tsx
@@ -1,3 +1,31 @@
-export default async function ProjectPage() {
-  return <div>Single Proect Page</div>;
+import { cookies } from "next/headers";
+import { getUserFromCookie } from "@/lib/auth";
+import { db } from "@/lib/db";
+import TasksCard from "@/components/TasksCard";
+
+const getData = async (id: string) => {
+  const user = await getUserFromCookie(cookies());
+
+  const project = await db.project.findFirst({
+    where: {
+      id,
+      ownerId: user?.id,
+    },
+    include: {
+      tasks: true,
+    },
+  });
+
+  return project;
+};
+
+export default async function ProjectPage({ params }: { params: any }) {
+  const project = await getData(params.id as string);
+
+  return (
+    <div className="h-full overflow-y-auto pr-6 w-1/1">
+      {/* @ts-ignore */}
+      {project && <TasksCard tasks={project.tasks} title={project.name} />}
+    </div>
+  );
 }
