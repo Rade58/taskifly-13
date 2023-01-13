e12de76531c6196ea651f9e6e249c8e8c7295bb0
Rade58 <bajic.rade2@gmail.com>
2023-01-13 19:42:42 +0100

    🦉

--- components/NewProject.tsx
+++ components/NewProject.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FC, useState } from "react";
+import { FC, useState, FormEvent } from "react";
 import Modal from "react-modal";
 import { createNewProject } from "@/lib/api";
 import Button from "./Button";
@@ -13,7 +13,34 @@ const NewProject: FC = () => {
   const closeModal = () => setIsOpen(false);
   const [name, setName] = useState("");
 
-  return null;
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await createNewProject(name);
+    closeModal();
+  };
+
+  return (
+    <div className="px-6 py-8 hover:scale-105 transition-all ease-in-out duration-200 flex justify-center items-center">
+      <Button onClick={() => openModal()}>+ New Project</Button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        overlayClassName="bg-[rgba(0,0,0,.4)] flex justify-center items-center absolute top-0 left-0 h-screen w-screen"
+        className="w-3/4 bg-white rounded-xl p-8"
+      >
+        <h1 className="text-3xl mb-6">New Project</h1>
+        <form className="flex items-center" onSubmit={handleSubmit}>
+          <Input
+            placeholder="project name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          {/* @ts-ignore */}
+          <Button type="submit">Create</Button>
+        </form>
+      </Modal>
+    </div>
+  );
 };
 
 export default NewProject;
